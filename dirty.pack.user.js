ab6eb349d5178fac8615906d5a540fc6bd1dabf8
crea7or <pavel.sokolov@gmail.com>
2010-11-16 00:06:42 +0300

    Online users table margin-top

--- dirty.pack.user.js
+++ dirty.pack.user.js
@@ -2254,6 +2254,7 @@ if ( _$.settings.online_enabled =='1' )
 			{
 				var checkinsMarkup = localStorage.getItem('checkinsMarkup');
 				var newdiv = document.createElement('div');
+				newdiv.setAttribute('style','margin-top: 20px');
 				newdiv.innerHTML =  checkinsMarkup;
 				divContentLeft.appendChild( newdiv );
 			}
