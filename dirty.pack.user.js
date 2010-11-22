37b38aacac4ade08ee99f9182a48b6e32b1d976d
Stasik0 <sten.gruener@gmail.com>
2010-11-22 22:56:25 +0100

    final working hook

--- dirty.pack.user.js
+++ dirty.pack.user.js
@@ -18,7 +18,7 @@
 var dateToCheck1 = new Date();
 
 var _$ = { 
-	buildtime: 1290462928,
+	buildtime: 1290462986,
 	settings: {},
 	settings_colors: "",
 	location: window.location.href.split(window.location.host)[1],
