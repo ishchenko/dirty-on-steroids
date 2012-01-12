8ae10c99e4b0ee48a8ea8988707714b6b43e89a8
Alexey Kulentsov <crimaniak@gmail.com>
2012-01-12 05:30:24 +0200

    Hidden rating added

---
+++ extra/no.rating.js
@@ -0,0 +1,23 @@
+// Спрятать рейтинг постов
+d3.addModule(
+{
+	type: "Социализм",
+	name: 'Пряталки рейтинга',
+	author: 'crimaniak',
+	config:
+		{postRating:   {type:'checkbox',value:false,caption:'Спрятать рейтинг постов'}
+		,commentRating:{type:'checkbox',value:false,caption:'Спрятать рейтинг комментариев'}
+		,voteButtons:  {type:'checkbox',value:false,caption:'Спрятать кнопки голосования'}
+		},
+		
+	run: function()
+	{
+		with(this.config)
+		{
+			if(postRating.value) $j('.post .vote_result').html('');
+			if(commentRating.value) $j('.c_vote .vote_result').html('');
+			if(voteButtons.value) $j('.vote_button').remove();
+		}
+	}
+
+});
