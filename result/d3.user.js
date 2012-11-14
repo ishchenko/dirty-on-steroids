7c04b492105315e0ec9d903a99b18db4c43ddf2f
Alexey Kulentsov <crimaniak@gmail.com>
2012-11-14 02:07:34 +0200

    Fix Post class for d3.ru

--- result/d3.user.js
+++ result/d3.user.js
@@ -471,9 +471,9 @@ var Post=function(container)
 	this.container=container;
 	this.container.get(0).post=this;
 	this.info=$j('.dd',this.container);
-	var a=$j('a[href^="/comments/"]',this.info);
+	var a=$j('a[href*="/comments/"]',this.info);
 	this.id=parseInt(this._idMask.exec(a.length<1 ? document.location.href : a.first().attr('href'))[1],10);
-	this.userName = $j('a[href^="/user/"]',this.info).text();
+	this.userName = $j('a[href*="/user/"]',this.info).text();
 	this.isNew = $j('a[href*="#new"]',this.info).length;
 	this.isMine = this.userName==d3.user.name;
 };
