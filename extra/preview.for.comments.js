bf6e7ee39b540fc0774c6dc159bebb0ba7e39b8c
crea7or <pavel.sokolov@gmail.com>
2012-11-15 11:30:17 +0400

    Preview for comments has been repaired

--- extra/preview.for.comments.js
+++ extra/preview.for.comments.js
@@ -9,43 +9,79 @@ d3.addModule(
 	run: function()
 	{
 		// div.comments_add_yarrr
-		var previewCont = document.querySelector('div.comments_add_pics');
+		var previewCont = document.querySelector('div.b-textarea_editor');
 		if ( previewCont )
 		{
-			previewCont.parentNode.insertBefore(d3.newDiv(
-				{style:{marginRight: '30px', float: 'right'}
-				,innerHTML:'<a href="#" id="prevLink" class="dashed" style="color: black; font-size: 11px;">предпросмотр</a>'
-			}), previewCont);
+			newLink = document.createElement('a');
+			newLink.href = '#';
+			newLink.id = 'prevLink';
+			newLink.innerHTML = 'Preview';
+			previewCont.appendChild( newLink );
 
-			$j('#prevLink').click( function(e){
-				var previewCont = document.getElementById('js-comments_reply_block');
-				var previewTextArea = document.getElementById('comment_textarea');
-				if ( previewTextArea && previewCont )
-				{
-					var previewDiv = document.getElementById('sp3previewDiv');
-					if ( previewDiv == null )
+			if ( document.location.href.indexOf('/inbox/write/') == -1 )
+			{
+				$j('#prevLink').click( function(e){
+					var previewCont = document.querySelector('div.b-comments_reply_block');
+					var previewTextArea = document.querySelector('textarea.i-form_text_input');
+					if ( previewTextArea && previewCont )
 					{
-						previewDiv = document.createElement('div');
-						previewDiv.setAttribute('style', 'padding: 5px 5px 5px 5px; margin-left: 0px !important; border: 1px dashed grey;');
-						previewDiv.setAttribute('id', 'sp3previewDiv');
-						previewDiv.setAttribute('class', 'comment');
-						previewCont.appendChild( previewDiv );
+						var previewDiv = document.getElementById('sp3previewDiv');
+						if ( previewDiv == null )
+						{
+							previewDiv = document.createElement('div');
+							previewDiv.setAttribute('style', 'padding: 5px; margin: 5px 0px 0px 0px !important; border: 1px dashed grey;');
+							previewDiv.setAttribute('id', 'sp3previewDiv');
+							previewDiv.setAttribute('class', 'comment');
+							previewCont.appendChild( previewDiv );
+						}
+						previewDiv.innerHTML = previewTextArea.value.replace(/\n/g,'<br>');
+						
 					}
-					previewDiv.innerHTML = previewTextArea.value.replace(/\n/g,'<br>');
-					
-				}
-				e.preventDefault();
-				return false;
-			});
+					e.preventDefault();
+					return false;
+				});
 
-			$j('#js-post-yarrr').click( function(e){
-  				var previewDiv = document.getElementById('sp3previewDiv');
-  				if( previewDiv )
-  				{
-  					previewDiv.parentNode.removeChild( previewDiv );
-  				}
-  				return true;
-			});
+				$j('input.b-comments_reply_block_yarrr').click( function(e){
+	  				var previewDiv = document.getElementById('sp3previewDiv');
+	  				if( previewDiv )
+	  				{
+	  					previewDiv.parentNode.removeChild( previewDiv );
+	  				}
+	  				return true;
+				});
+			}
+			else // wrinting new inbox
+			{
+				$j('#prevLink').click( function(e){
+					var previewCont = document.getElementById('js-new_inbox_form');
+					var previewTextArea = document.getElementById('js-new_inbox_body');
+					if ( previewTextArea && previewCont )
+					{
+						var previewDiv = document.getElementById('sp3previewDiv');
+						if ( previewDiv == null )
+						{
+							previewDiv = document.createElement('div');
+							previewDiv.setAttribute('style', 'padding: 5px; margin: 5px 0px 0px 0px !important; border: 1px dashed grey;');
+							previewDiv.setAttribute('id', 'sp3previewDiv');
+							previewDiv.setAttribute('class', 'comment');
+							previewCont.appendChild( previewDiv );
+						}
+						previewDiv.innerHTML = previewTextArea.value.replace(/\n/g,'<br>');
+						
+					}
+					e.preventDefault();
+					return false;
+				});
+
+				$j('#js-new_inbox_submit').click( function(e){
+	  				var previewDiv = document.getElementById('sp3previewDiv');
+	  				if( previewDiv )
+	  				{
+	  					previewDiv.parentNode.removeChild( previewDiv );
+	  				}
+	  				return true;
+				});
+			}
 		}
 	},
 });
