8e252537ed50fac90c8f96148100b38e86e1fd18
crea7or <pavel.sokolov@gmail.com>
2012-11-19 21:50:48 +0400

    d3search fix for d3.ru

--- extra/d3.search.js
+++ extra/d3.search.js
@@ -1,12 +1,35 @@
-// Redirect search to d3search.ru
 d3.addModule(
 {
 	type: "Поиск",
 	name: 'Поиск на d3search.ru',
-	author: 'crimaniak',
+	author: 'crimaniak, crea7or',
 	config: {active:{type:'checkbox',value:true}},
 	run: function()
 	{
-		$j('form[name="simple-search"]').attr('action','http://d3search.ru/search');
+		var script2run = document.createElement('script');
+		script2run.type = 'text/javascript';
+		script2run.text = "searchHandler = {"
+			+ "submitQuery : function (search_form) {"
+			+ "	if (search_form.getParent('.b-header_search')) {"
+			+ "		var input_holder = $('js-header_search_input').getParent('.b-header_search_input_holder');"
+			+ "		var search_holder = $('js-header_search_input').getParent('.b-header_search');"
+			+ "		if (search_holder.hasClass('b-header_search_input_shrinked')) {"
+			+ "			search_holder.removeClass('b-header_search_input_shrinked');"
+			+ "			input_holder.get('morph').removeEvents('complete');"
+			+ "			input_holder.set('morph', {duration:333, onComplete : function () {"
+			+ "				$('js-header_search_input').focus();"
+			+ "				(function () {document.addEvent('click', searchHandler.shrinkHeaderSearch);}).delay(200); }}); input_holder.morph({ width : 250 });"
+			+ "		} else {	var query = $(search_form).getElement('input[type=\"text\"]').value; window.location.href = 'http://d3search.ru/search?query=' + encodeURIComponent(query);}"
+			+ "	} else { var query = $(search_form).getElement('input[type=\"text\"]').value; window.location.href = 'http://d3search.ru/search?query=' + encodeURIComponent(query); }},"
+			+ "shrinkHeaderSearch : function () {"
+			+ "	var input_holder = $('js-header_search_input').getParent('.b-header_search_input_holder');"
+			+ "	var search_holder = $('js-header_search_input').getParent('.b-header_search');"
+			+ "	input_holder.get('morph').removeEvents('complete');"
+			+ "	input_holder.set('morph', {duration:333, onComplete : function () {"
+			+ "		search_holder.addClass('b-header_search_input_shrinked'); }});"
+			+ "	input_holder.morph({ width : 0 }); document.removeEvent('click', searchHandler.shrinkHeaderSearch); }};";
+
+		head = (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]);
+		head.appendChild( script2run );
 	}
 });
