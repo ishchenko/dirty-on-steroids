53cde7372d25d37210180ef5be6c7d60ed03d063
Aivean <aiveeen@gmail.com>
2012-11-27 00:53:12 +0200

    #26 onPost and onComment support for hidden.author module. +bugfixes

--- extra/hidden.author.js
+++ extra/hidden.author.js
@@ -1,4 +1,3 @@
-
 // Hidden authors
 d3.addModule(
 {
@@ -9,57 +8,63 @@ d3.addModule(
 			,comments:{type:'checkbox',value:false,caption:"Анонимные комментаторы"}
 			,mark:	  {type:'text',value:'?????',caption:"Менять никнеймы на:"}
 			},
+
+	onPost: function(post){
+		// anonymize posts
+		if(this.config.posts.value) {
+			$j('.dd a[href^="/user/"]', post.container).html(this.config.mark.value);
+		}
+	},
+
+	onComment: function(comment) {
+		var mark=this.config.mark.value;
+		// anonymize comments
+		if(this.config.comments.value) {
+			// footers
+			$j('.c_footer a[href*="/user/"]', comment.container).html(mark);
+			// bodies
+			d3.xpath.each("//div[contains(@class,'c_body')]//text()", //text()[matches(string(),'[^ ]+:[\\s\\S]*')]",
+				function(node)
+				{
+					if(node.data != undefined ///< opera fix
+						&& node.parentNode.className.split(/\s/).indexOf("c_body") >= 0) //TODO maybe this can be replaced by XPATH
+						node.data=node.data.replace(/^[^ ]+:/,mark+':');
+				}, comment.container.get(0));
+		}
+	},
+
 	run: function()
 	{
-		// anonymize posts 
-		if(this.config.posts.value)
-			$j('.post .dd a[href^="/user/"]').html(this.config.mark.value);
-
-		var mark=this.config.mark.value;
-		
-		// anonymize comments
-		if(this.config.comments.value)
-		{
-			// footers
-			$j('.comment .c_footer a[href^="/user/"]').html(mark);
-			// bodies
-			d3.xpath.each("//div[contains(@class,'c_body')]//text()", //text()[matches(string(),'[^ ]+:[\\s\\S]*')]", 
-					function(node)
-					{
-						if(node.data != undefined) ///< opera fix
-							node.data=node.data.replace(/^[^ ]+:/,mark+':');
+		if (this.config.comments.value) {
+			var mark = this.config.mark.value;
+			var searchMask = new RegExp(RegExp.escape(mark) + ': ', 'g');
+			with (d3.window.commentsHandler) {
+				var old = toggleCommentForm;
+				toggleCommentForm = function (button, postId, commentId) {
+					old(button, postId, commentId);
+					var $textarea = $j('.b-comments_reply_block textarea');
+					// remove ?????: from answer form
+					$textarea.each(function() {
+						this.value = this.value.replace(searchMask, '');
 					});
 
-			// remove ?????: from answer form
-			var searchMask=new RegExp(RegExp.escape(mark)+': ','g');
-			with(d3.window.commentsHandler)
-			{
-				var old=toggleCommentForm;
-				toggleCommentForm=function(button,id)
-				{
-					old(button,id);
-					with($j('#comment_textarea').get(0))
-						value=value.replace(searchMask,'');
+					// add username back to answer form before submit
+					var $yarr = $j("input.b-comments_reply_block_yarrr");
+					$yarr.on("click.hidden.author", function($button, $textarea){
+						return function(){
+							var $user = $j("a.c_user", $textarea.parents("div.comment"));
+							if ($user.length) {
+								var href = $user.attr("href");
+								var userName = href.substr(href.lastIndexOf('/') + 1);
+								$textarea.each(function () {
+									this.value = userName + ": " + this.value;
+								});
+							}
+							$button.off("click.hidden.author");
+						}
+					} ($yarr, $textarea));
 				};
 			}
 		}
-		
-		// Don't work! I don't know why.
-		// replace ????? back to nickname before sending form
-		/*
-		var mask2=new RegExp('^'+RegExp.escape(mark)+':','g');
-		$j('#js-comments_reply_form').submit(function(event)
-			{
-				with(event.target)
-				{
-					var r=elements['replyto'].value;
-					if(r)
-					{
-						var u=$j('#'+r).get(0).comment.userName;
-						elements['comment'].value=elements['comment'].value.replace(mask2,u+':');
-					}
-				}
-			});
-			*/
 	}
 });
