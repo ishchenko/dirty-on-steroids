b72159340a0f879ac6fbe5845f682b0b23a457e7
Stasik0 <github@stasik.com>
2012-11-26 15:34:41 +0100

    fixes

--- extra/redirect.to.new.js
+++ extra/redirect.to.new.js
@@ -1,4 +1,4 @@
-// Redirect from / to /new
+// Total redirect from / to /new
 d3.addModule(
 {
 	type: "Прочее",
@@ -8,8 +8,25 @@ d3.addModule(
 
 	run: function()
 	{
-		if(document.location.pathname == '/')
+		if(document.location.pathname == '/' && document.location.href.indexOf('?')==-1){
 			document.location.href = document.location.href+"new";
+			return;
+		}
+
+		//modify old links
+		$j('a[href$="d3.ru"], a[href$="d3.ru/"], a[href="/"]').not('a[href^="mailto:"]').not('a[class="b-blog_nav_sort_link"]').each(function(link){
+			var href = $j(this).attr('href');
+			if(href.indexOf('write')==-1){
+				if(href.endsWith('/')){
+					$j(this).attr('href', $j(this).attr('href')+'new');
+				}else{
+					$j(this).attr('href', $j(this).attr('href')+'/new');
+				}
+			}
+		});
+		$j('a.b-blog_nav_sort_link[href="/"]').each(function(link){
+			$j(this).attr('href', $j(this).attr('href')+'?');
+		});
 	}
 
 });
