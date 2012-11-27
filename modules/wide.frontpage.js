55bb47d0f1c0e7cbd0246af883f5599f8acc62ae
Aivean <aiveeen@gmail.com>
2012-11-28 01:17:05 +0200

    Refactor wide.frontpage module.

--- modules/wide.frontpage.js
+++ modules/wide.frontpage.js
@@ -5,20 +5,9 @@ d3.addModule(
 	name: 'Широкая главная',
 	author: 'crea7or',
 	config: {active:{type:'checkbox',value:true}},
-	
-	run: function()
-	{
-		var postHolder = document.getElementById('js-posts_holder');
-		if ( postHolder )
-		{
-			var banner = document.getElementById('js-random_interests');
-			if ( banner )
-			{
-				banner.parentNode.removeChild( banner );
-			}
-			postHolder.setAttribute('style', 'padding-right: 30px;');
-			postHolder.setAttribute('class', '');
-		}
-	},
+
+	run: function () {
+		$j("#js-posts_holder").attr("style", "padding-right: 30px;").attr("class", "");
+		$j("#js-random_interests").remove();
+	}
 });
-	
