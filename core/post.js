9691e734159fce7f4ad73f2aa890eb9ed633d00d
Aivean <aiveeen@gmail.com>
2012-11-28 00:42:30 +0200

    Fix #29 Post object initialization in inbox

--- core/post.js
+++ core/post.js
@@ -5,7 +5,8 @@ var Post=function(container)
 	this.container.get(0).post=this;
 	this.info=$j('.dd',this.container);
 	var a=$j('a[href*="/comments/"]',this.info);
-	this.id=parseInt(this._idMask.exec(a.length<1 ? document.location.href : a.first().attr('href'))[1],10);
+	var execResult = this._idMask.exec(a.length<1 ? document.location.href : a.first().attr('href'));
+	this.id=execResult?parseInt(execResult[1],10):0;
 	this.userName = $j('a[href*="/user/"]',this.info).text();
 	this.isNew = $j('a[href*="#new"]',this.info).length;
 	this.isMine = this.userName==d3.user.name;
