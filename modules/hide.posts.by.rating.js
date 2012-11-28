c16fe32b0bfd3504c1f3af2cd209e62a5c3a39c3
Aivean <aiveeen@gmail.com>
2012-11-28 11:25:13 +0200

    #26 onPost support for hide.posts.by.rating module.

--- modules/hide.posts.by.rating.js
+++ modules/hide.posts.by.rating.js
@@ -1,22 +1,21 @@
 // Hides posts with low rating
 d3.addModule(
-    {
-        type: "Содержание",
-        name: 'Прятать посты с низким рейтингом',
-        author: 'Aivean',
-        config: {
-            active: {type: 'checkbox', value: false},
-            rating: {
-                type: 'select', value: 0,
-                options: {'10': 10, '5': 5, '0': 0, '-5': -5, '-10': -10},
-                caption: 'Рейтинг меньше, чем:'
-            }
-        },
-        run: function () {
-        	d3.content.posts.forEach(function(p) {
-        		if (p.ratingValue() < this.config.rating.value) {
-                    p.container.hide();
-                }
-        	}, this);
-        }
-    });
+	{
+		type: "Содержание",
+		name: 'Прятать посты с низким рейтингом',
+		author: 'Aivean',
+		config: {
+			active: {type: 'checkbox', value: false},
+			rating: {
+				type: 'select', value: 0,
+				options: {'100': 100, '40': 40, '10': 10, '5': 5, '0': 0, '-5': -5, '-10': -10},
+				caption: 'Рейтинг меньше, чем:'
+			}
+		},
+
+		onPost: function (post) {
+			if (post.ratingValue() < this.config.rating.value) {
+				post.container.hide();
+			}
+		}
+	});
