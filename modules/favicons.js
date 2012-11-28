49a30d623c48bd1c45101aba03b0809faa0b5f6a
Aivean <aiveeen@gmail.com>
2012-11-29 00:47:19 +0200

    #26 onPost onComment support for favicons module. fix #33

--- modules/favicons.js
+++ modules/favicons.js
@@ -8,7 +8,7 @@ d3.addModule(
 		active:{type:'checkbox',value:true},
 		mouseover:{type:'radio',caption:'Показывать иконки',options:{"перед ссылками":"false","при наведении":"true"},value:"false"},
 		domainWhitelist:{type: 'text', caption:'Список доменов', value:'dirty.ru,d3.ru,d3search.ru,livejournal.com,lenta.ru,flickr.com,google.com,google.ru,yandex.ru,yandex.net,rian.ru,wikipedia.org,wikimedia.org,futurico.ru,leprosorium.ru,lepra.ru,facebook.com,twitter.com,gazeta.ru,vedomosti.ru,1tv.ru,fontanka.ru,kommersant.ru,vesti.ru,kp.ru,blogspot.com,narod.ru,vimeo.com,rbc.ru,korrespondent.net,youtube.com'
-		},
+		}
 	},
 	
 	getDomainMasks: function()
@@ -16,16 +16,8 @@ d3.addModule(
 		return this.config.domainWhitelist.value
 			.split(/[\s,]+/)
 			.map(function(item){
-				return new RegExp('^(.*\.)?'+item.replace(/\./g,'\\.').replace(/\*/g,'.*')+'$','i');
+				return new RegExp('^(.*\\.)?'+item.replace(/\./g,'\\.').replace(/\*/g,'.*')+'$','i');
 			});
-	},
-	
-	hideFavicon: function(e){
-		$j(e.target).css('background-image', 'none');
-	},
-
-	showFavicon: function(e, faviconUrl){
-		$j(e.target).css({'padding-top':'16px', 'background-image':'url('+faviconUrl+')', 'background-repeat':'no-repeat'});
 	},
 	
 	inWhiteList: function(domain){
@@ -44,31 +36,61 @@ d3.addModule(
 		}
 	},
 
+	showFavicon: function (el, faviconUrl) {
+		el.css('background-image','url('+faviconUrl+')').addClass("faviconized");
+	},
+
+	hideFavicon: function (el) {
+		el.css('background-image','none').removeClass("faviconized");
+	},
+
 	run: function(){
 		if(d3.page.user) return;
+
+		$j("<style type='text/css'> \n\
+				.faviconized { \n\
+				 	padding-left:19px; \n\
+				    background-repeat:no-repeat; \n\
+					background-position:left center; \n\
+				} \n\
+			</style>").appendTo("head");
+
+
+	},
+
+	onPost: function(post) {
+		this.processItem(post.container);
+	},
+
+	onComment: function(comment) {
+		this.processItem(comment.container);
+	},
+
+	processItem: function(container) {
 		var me=this;
 		//iterate over links
-		$j.each($j('div.dt > h3 > a').not('a[href*="'+window.location.hostname+'"]').add('div.dt > a, div.c_body > a, div.dt > div.post_video > div > a').not('a[class*="b-controls_button"]'), function(index, link){
-			if(me.inWhiteList(link.hostname))
-			{
-				if(link.hostname.indexOf('d3.ru', link.hostname.length - 'd3.ru'.length)!==-1){
-					//yandex has no d3.ru icon yet
-					var faviconUrl = 'http://www.google.com/s2/favicons?domain='+link.hostname;				
-				}else{
-					var faviconUrl = 'http://favicon.yandex.net/favicon/'+link.hostname;
-				}
+		$j.each($j('div.dt > h3 > a', container).not('a[href*="' + window.location.hostname + '"]')
+			.add('div.dt a, div.c_body a, div.dt div.post_video a', container).not('a[class*="b-controls_button"]'),
+			function (index, link) {
+				if (me.inWhiteList(link.hostname)) {
+					var faviconUrl;
+					if (link.hostname.indexOf('d3.ru', link.hostname.length - 'd3.ru'.length) !== -1) {
+						//yandex has no d3.ru icon yet
+						faviconUrl = 'http://www.google.com/s2/favicons?domain=' + link.hostname;
+					} else {
+						faviconUrl = 'http://favicon.yandex.net/favicon/' + link.hostname;
+					}
 
-				if(me.config.mouseover.value == 'true'){
-					$j(link)
-						.mouseover(function(e){me.showFavicon(e, faviconUrl);})
-						.mouseout(me.hideFavicon);
-				}else{
-					$j(link).css('padding-left','19px')
-							.css('background-repeat','no-repeat')
-							.css('background-image','url('+faviconUrl+')')
-							.css('background-position','left center');
+					if (me.config.mouseover.value == 'true') {
+						$j(link).mouseover(function () {
+							me.showFavicon($j(this), faviconUrl);
+						}).mouseout(function () {
+								me.hideFavicon($j(this))
+							});
+					} else {
+						me.showFavicon($j(link), faviconUrl);
+					}
 				}
-			}
-		});
+			});
 	}
 });
