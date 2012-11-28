117523b5754c5bd88a8bbb540a422d8d7dce734b
Aivean <aiveeen@gmail.com>
2012-11-28 11:37:39 +0200

    #26 onPost support for kdpv module.

--- modules/kdpv.js
+++ modules/kdpv.js
@@ -6,22 +6,19 @@ d3.addModule(
 	author: 'crimaniak',
 	config: {active:{type:'checkbox',value:false}},
 		
-	run: function()
+	onPost: function(post)
 	{
-		var items=d3.content.posts;
-		
-		if(items.length>1)
-			for(var i=0;i<items.length;++i)
-				this.process(items[i]);
-	},
-
-	process: function(item)
-	{
-		var img=$j('p img',item.container).first();
-		var p=img.parent();
-		var opener=$j('<a href="#">Показать картинку</a>');
-		img.css('display','none');
-		opener.prependTo(p);
-		p.click(function(){img.css('display','');opener.remove();return false;});
+		var img = $j('.post_body img', post.container).first();
+		if (img.length) {
+			var p = img.parent();
+			var opener = $j('<a href="#">Показать картинку</a>');
+			img.css('display', 'none');
+			opener.prependTo(p);
+			p.click(function () {
+				img.css('display', '');
+				opener.remove();
+				return false;
+			});
+		}
 	}
 });
