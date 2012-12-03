8453682f567f1d83740772730578130db24e0ce5
crea7or <pavel.sokolov@gmail.com>
2012-12-03 05:21:59 +0400

    #46 text-decoration: none as seen on lepra

--- modules/highlight.subsite.url.js
+++ modules/highlight.subsite.url.js
@@ -20,10 +20,10 @@ d3.addModule(
 		styleName: "subsite-link-highlighted",
 		styles: {
 			'border': "border: 1px solid #888888;\n" +
-				"padding: 0px 2px 1px 2px;" +
+				"padding: 0px 2px 1px 2px; text-decoration: none;" +
 				"-webkit-border-radius: 2px; -moz-border-radius: 2px; border-radius: 2px;",
 			'yellow-back': "background-color: #E9DFC3;" +
-				"padding: 0px 2px 1px 2px;"
+				"padding: 0px 2px 1px 2px; text-decoration: none;"
 		},
 
 		run: function() {
