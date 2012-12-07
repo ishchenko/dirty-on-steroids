4312f5b5ed0222b63245a73282fa3b63d1430e23
crea7or <pavel.sokolov@gmail.com>
2012-12-08 03:26:35 +0400

    new module - recreate inbox button

---
+++ modules/recreate.inbox.js
@@ -0,0 +1,81 @@
+// Кнопка пересоздания инбокса
+d3.addModule(
+{
+	type: "Содержание",
+	name: 'Добавлять кнопку пересоздания инбокса',
+	author: 'crea7or',
+	config: {active:{type:'checkbox',value:1}},
+	
+	run: function()
+	{
+		var me = this;
+		var tortAddLinksInbox = document.querySelector('div.b-inbox_controls');
+		if ( tortAddLinksInbox )
+		{
+			tortNewd = document.createElement('div');
+			tortNewa = document.createElement('a');
+			tortNewa.href = '#';
+			tortNewd.setAttribute('style', 'margin-top: 10px;');
+			tortNewa.setAttribute('class', 'b-inbox_controls_delete_inbox');
+			tortNewa.innerHTML = 'Пересоздать инбокс';
+			tortNewd.appendChild( tortNewa );
+			tortAddLinksInbox.appendChild( tortNewd );
+			$j(tortNewa).bind('click', function(e) { me.copyAction(); });
+		}
+
+		if ( document.location.href.indexOf('d3.ru/my/inbox/write/') > -1 )
+		{
+			
+			var timeOfUsersCopy = localStorage.getItem('dirtySpmRecInbTime');
+			if ( timeOfUsersCopy )
+			{
+				if (( new Date().getTime() - timeOfUsersCopy ) < 1000 * 30 ) // 10 sec
+				{
+					var inbUsers = document.getElementById('js-new_inbox_to');
+					if ( inbUsers )
+					{
+						inbUsers.value = localStorage.getItem('dirtySpmRecInbUsers');
+					}
+					var inbText = document.getElementById('js-new_inbox_body');
+					if ( inbText )
+					{
+						inbText.value = localStorage.getItem('dirtySpmRecTxt');
+					}
+				}
+				localStorage.removeItem('dirtySpmRecInbTime');
+				localStorage.removeItem('dirtySpmRecInbUsers');
+				localStorage.removeItem('dirtySpmRecTxt');
+			}
+		}
+	},
+
+	copyAction: function(e)
+	{		
+		var inboxUsers = document.getElementById('js-inbox_controls_users');
+		if ( inboxUsers )
+		{
+			var linksToUsers = inboxUsers.getElementsByTagName('a');
+			var userNames = new Array();
+			if ( linksToUsers )
+			{
+				for ( var i = 0; i < linksToUsers.length; i++)
+				{
+					userNames.push( linksToUsers[i].textContent );
+				}
+			}
+			if ( userNames.length > 0 )
+			{
+				var postTxtDiv = document.querySelector('div.post_body');
+				if ( postTxtDiv)
+				{
+					localStorage.setItem('dirtySpmRecTxt', postTxtDiv.innerHTML);
+				}				
+				localStorage.setItem('dirtySpmRecInbUsers', userNames.join());
+				localStorage.setItem('dirtySpmRecInbTime', new Date().getTime());
+				document.location.href = 'http://d3.ru/my/inbox/write/';
+			}
+		}
+		e.preventDefault();
+		return false;
+	},
+});
