29f7441d8abac5865afd1390655ff6a1538cc33b
alexzabolotny <alexander.zabolotny@gmail.com>
2012-12-12 18:04:44 -0500

    Opera MSP: new way of building extension - using maximum of BuildExtension class. Extension version is populated into config.xml using build number as minor part.

--- build/buildOperaExtension.php
+++ build/buildOperaExtension.php
@@ -1,50 +1,98 @@
-#!/usr/bin/php
 <?php
-/**
- * From @alexzabolotny:
- * Apparently, this build script will work only under Mac OS or Linux, or other system with bash commands.
- * Cygwin should work too, but i haven't tested.
- */
 
 error_reporting(E_ALL);
 ini_set('display_errors', true);
 
-// copy files into temp folder
-$srcDir = __DIR__."/../extensions/opera/dirtyMSP/";
-$payloadFromPath = __DIR__."/../result/d3.user.js";
-$payloadToPath = $srcDir."/includes/payload.js";
+include "BuildExtension.php";
 
-$payloadTemplate = file_get_contents($srcDir."/../payload.template.js");
-file_put_contents(
-	$payloadToPath,
-	str_replace(
-		array(
-			"//@@payload@@",
-		),
-		array(
-			file_get_contents($payloadFromPath),
-		),
-		$payloadTemplate
-	)
-);
+class BuildOperaExtension extends BuildExtension {
+    const EXT_DIR                   = "extensions/opera/dirtyMSP/";
+    const EXT_FILE_NAME_TEMPLATE    = "dirtyMSP-%s.oex";
+    const SUPER_VERSION             = 1;
 
-$tmpSrcDir = trim(shell_exec("mktemp -d -t fb-image-build.XXXXXXXXX"));
-$productFileName = "dirtyMSP-%s.oex";
+    private $tmpPath                = null;
 
-echo "Copying sources into {$tmpSrcDir} ...".PHP_EOL;
-shell_exec("cp -r {$srcDir} {$tmpSrcDir}");
+    public function getExtDir() {
+        return $this->getRootDir().self::EXT_DIR;
+    }
 
-// parse config to find out the version
-$extensionConfig = simplexml_load_file($tmpSrcDir."/config.xml");
-$attributes = $extensionConfig->attributes();
-$extensionVersion = $attributes['version'][0];
+    public function getExtTmpDir() {
+        if (is_null($this->tmpPath)) {
+            $this->tmpPath = trim(shell_exec("mktemp -d -t fb-image-build.XXXXXXXXX"));
+        }
 
-// make zip, copy, rename
-$packageName = $tmpSrcDir."/".sprintf($productFileName, $extensionVersion);
+        return $this->tmpPath.DIRECTORY_SEPARATOR;
+    }
 
-echo "Packing into {$packageName} ...".PHP_EOL;
-shell_exec("cd {$tmpSrcDir}; zip -r -9 {$packageName} *");
-shell_exec("mv {$packageName} {$srcDir}/../../../result/");
-echo "Placed package into ".realpath($srcDir."/../../result/").PHP_EOL;
+    public function copyCode() {
+        shell_exec("cp -r {$this->getExtDir()} {$this->getExtTmpDir()}");
 
-exit(0);
+        shell_exec("cp {$this->getSourceJsFilePath()} {$this->getResJsFilePath()}");
+    }
+
+    private function getExtFileName() {
+        return sprintf(self::EXT_FILE_NAME_TEMPLATE, $this->getVersion());
+    }
+
+    private function getResBundleFilePath() {
+        return $this->getResultDir().$this->getExtFileName();
+    }
+
+    public function getExtFilePath() {
+        return $this->getExtTmpDir().$this->getExtFileName();
+    }
+
+    public function getResJsFilePath() {
+        return $this->getExtTmpDir()."includes/payload.js";
+    }
+
+    public function getManifestFilePath() {
+        return $this->getExtTmpDir()."config.xml";
+    }
+
+    public function packExtension() {
+        shell_exec("cd {$this->getExtTmpDir()}; zip -r -9 {$this->getExtFilePath()} *");
+    }
+
+    public function populateConfig() {
+        $config = file_get_contents($this->getManifestFilePath());
+        $configWithVersion = str_replace("@version@", $this->getVersion(), $config);
+        file_put_contents($this->getManifestFilePath(), $configWithVersion);
+    }
+
+    public function moveProductToResultFolder() {
+        unlink($this->getResBundleFilePath());
+        copy($this->getExtFilePath(), $this->getResultDir().$this->getExtFileName());
+    }
+
+    public function build($arg) {
+        $this->echoPaths();
+
+        $this->copyCode();
+
+        $this->populateConfig();
+
+        $this->packExtension();
+
+        $this->moveProductToResultFolder();
+
+        $bundle = $this->getResBundleFilePath();
+
+        if (file_exists($bundle)) {
+            echo PHP_EOL;
+            echo "New extension created at: {$bundle}".PHP_EOL;
+            echo "Enjoy!".PHP_EOL;
+        } else {
+            echo "Something went wrong, please check the output above.".PHP_EOL;
+        }
+    }
+
+    public function getVersion() {
+        $buildNumber = $this->getBuildNumber();
+
+        return self::SUPER_VERSION.".".$buildNumber;
+    }
+}
+
+$builder = new BuildOperaExtension();
+$builder->build(@$argv[1]);
