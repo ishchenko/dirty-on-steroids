b072e10a9e84e866e53a7a446dd905b0778b01dd
Alexey Kulentsov <crimaniak@gmail.com>
2012-12-26 01:12:18 +0200

    Fix bug with inbox id

--- core/post.js
+++ core/post.js
@@ -3,10 +3,9 @@ var Post=function(container)
 {
 	this.container=container;
 	this.container.get(0).post=this;
+
+	this.id = parseInt(container.attr('id').substr(1), 10);
 	this.info=$j('.dd',this.container);
-	var commentsLink=$j('a[href*="/comments/"]',this.info);
-	var execResult = this._idMask.exec(commentsLink.length<1 ? document.location.href : commentsLink.first().attr('href'));
-	this.id=execResult?parseInt(execResult[1],10):0;
 	this.userName = $j('a[href*="/user/"]',this.info).text();
 	this.isNew = $j('a[href*="#new"]',this.info).length || $j(".b-all_new_comments_link" ,this.info).length;
 	this.isMine = this.userName==d3.user.name;
