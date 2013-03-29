80133e172c09849118f05233a10fc2ac9da3e247
Alexey Kulentsov <crimaniak@gmail.com>
2013-03-30 01:56:09 +0200

    Optional update request, compile

--- modules/update.link.js
+++ modules/update.link.js
@@ -6,6 +6,7 @@ d3.addModule(
 	name: 'Линк на последнюю версию',
 	author: 'crimaniak',
 	config: {
+		checkUpdates:{type:'checkbox', value: 1, caption: 'проверять наличие новых версий', description: 'Если эта опция активна, время от времени сервис-пак проверяет наличие обновлений и сообщает о них, а также спрашивает разрешение на обновление.'},
 		ver:{type:'html', value:'Текущая версия: '+d3.buildNumber+' '+d3.buildMode+' от '+d3.buildTime},
 		link:{type:'html',value:'<a href="https://github.com/crimaniak/dirty-on-steroids/raw/master/result/d3.user.js">Установить последнюю версию скрипта</a>'}
 	},
@@ -13,6 +14,7 @@ d3.addModule(
 	interval: 3600000*6,
 	jsonLink: 'https://api.github.com/repos/crimaniak/dirty-on-steroids/contents/result/pack.version.json?callback=checkUpdate',
 	run: function(){
+		if(!this.config.checkUpdates.value) return;
 		var lastCheck = d3.storage.get(this.lastCheckName, {time: 0, result: ''});
 		var now = Date.now();
 		var me = this;
