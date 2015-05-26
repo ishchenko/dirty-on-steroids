387a4b0ee1e454d1fb78c838133af334d1dadedc
Aivean <aiveeen@gmail.com>
2015-05-26 12:33:51 -0700

    Fixed gender color in comments for lepra.

--- modules/gender.color.js
+++ modules/gender.color.js
@@ -1,36 +1,39 @@
 // Половой вопрос
-d3.addModule(
-{
+d3.addModule({
 	type: "Социализм",
 	name: 'Цветовая дифференциация полов',
 	author: 'crea7or',
-	config: {active:{type:'checkbox',value:1}},
-	
-	onPost: function(post){
+	config: {active: {type: 'checkbox', value: 1}},
+
+	onPost: function (post) {
 		this.processElements(post.info.get(0).querySelectorAll('a.c_user'));
 	},
 
-	onComment: function(comment){
+	onComment: function (comment) {
 		this.processElements(comment.getFooter().get(0).querySelectorAll('a.c_user'));
 	},
 
 	//expecting array of a.c_user DOM elements
-	processElements: function(elemArray){
-		if(!elemArray)return;
-		for(var i = 0; i < elemArray.length; i++)
-		{
-			if (elemArray[i].previousSibling.nodeValue.indexOf('Написала') > -1 )
-			{
-				//female
-				elemArray[i].style.borderBottom = '1px solid #ff78f7';
-			}
-			else
-			{
-				//male
-				elemArray[i].style.borderBottom = '1px solid #5086ff';
-			}
-			elemArray[i].style.textDecoration = 'none';
+	processElements: function (elemArray) {
+		if (!elemArray)return;
+
+		function testFemale(el) {
+			return el && el.textContent &&
+				el.textContent.indexOf('Написала') > -1;
 		}
-	},
+
+		for (var i = 0; i < elemArray.length; i++) {
+			var prev = elemArray[i].previousSibling;
+			if (prev) {
+				if (testFemale(prev) || testFemale(prev.previousSibling)) {
+					//female
+					elemArray[i].style.borderBottom = '1px solid #ff78f7';
+				} else {
+					//male
+					elemArray[i].style.borderBottom = '1px solid #5086ff';
+				}
+				elemArray[i].style.textDecoration = 'none';
+			}
+		}
+	}
 });
-	
